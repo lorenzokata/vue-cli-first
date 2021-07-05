f303b56c56003556875040def9391d673c63e91e
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-07-05 16:17:24 +0200

    add bonus

---
+++ vue-cli-first/src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="jumbotron">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.jumbotron {
+  margin: 0 0 40px;
+  background-image: url("../assets/warzone.jpg");
+  background-position: center;
+  height: 1000px;
+}
+</style>
