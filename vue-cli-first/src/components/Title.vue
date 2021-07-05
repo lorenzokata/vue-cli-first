dac8600b82aa764ed86038a7ef46d87d72ef9d39
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-07-05 15:49:03 +0200

    end?

---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+  color: red;
+  font-size: 60px;
+}
+</style>
